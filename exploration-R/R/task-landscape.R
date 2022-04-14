eb7d4aec4f990d045bd495953d168da7d571ea2e
Mirko Thalmann <mirkothalmann@hotmail.com>
2022-04-14 19:27:50 +0200

    add three visualizations of task landscape

--- exploration-R/R/task-landscape.R
+++ exploration-R/R/task-landscape.R
@@ -1,17 +1,31 @@
 library(tidyverse)
+library(factoextra)
+library(ggrepel)
+library(formattable)
+
+home_grown_stuff <- c("R/utils.R")
+walk(home_grown_stuff, source)
 
 # create a list with tasks used previously
 
 tbl_tasks <- tribble(
-  ~task_name,                          ~confound_rew_inf, ~nr_arms, ~add_gen, ~experience,
-  "observe-or-bet",                    FALSE,               "few",    FALSE,        TRUE,
-  "two-armed-bandit",                  TRUE,                "few",    FALSE,        TRUE,
-  "restless-bandit",                   TRUE,                "few",    FALSE,        TRUE,
-  "grid-task",                         TRUE,                "many",   TRUE,        TRUE,
-  "decision-from-experience",          TRUE,                "few",    FALSE,        TRUE,
-  "decision-from-experience-sampling", FALSE,               "few",    TRUE,       TRUE,
-  "decision-from-description",         FALSE,               "few",    FALSE,  FALSE
+  ~task_name,           ~nr_arms, ~moving_stats, ~add_gen, ~experience, ~confound_rew_inf, ~capacity,
+  "observe-or-bet",     "few",    FALSE,        FALSE,    TRUE,         FALSE,             1,
+  "two-armed-bandit",   "few",    FALSE,        FALSE,    TRUE,         TRUE,              1,
+  "restless-bandit",    "few",    TRUE,         FALSE,    TRUE,         TRUE,              2,
+  "grid-task",          "many",   FALSE,        TRUE,     TRUE,         TRUE,              3,
+  "dfe",                "few",    FALSE,        FALSE,    TRUE,         TRUE,              1,
+  "dfe-sampling",       "few",    FALSE,        FALSE,    TRUE,         FALSE,             1,
+  "dfd",                "few",    FALSE,        FALSE,    FALSE,        FALSE,             0,
+  "horizon-task",       "few",    FALSE,        FALSE,    FALSE,        FALSE,             0,
 )
 
-
 tbl_tasks <- format_task_tbl(tbl_tasks)
+
+# a task table
+plot_task_table(tbl_tasks)
+x11()
+plot_labels_task(tbl_tasks)
+x11()
+plot_pca(tbl_tasks = tbl_tasks)
+
