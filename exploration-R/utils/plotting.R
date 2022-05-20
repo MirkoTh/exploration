fc9d3b142e30eeb09072dbd052e5b5fd97370f71
Mirko Thalmann <mirkothalmann@hotmail.com>
2022-05-20 15:33:52 +0200

    facet histogram of reward distributions over variances

--- exploration-R/utils/plotting.R
+++ exploration-R/utils/plotting.R
@@ -5,16 +5,22 @@ check_reward_distributions <- function(tbl_conditions) {
   #' @param tbl_conditions tibble with information and rewards
   #' @return nothing; just plots
   
-  tbl_rewards <- map(tbl_conditions$rewards, ~ as.data.frame(.x)) %>%
-    map(~ pivot_longer(.x, everything())) %>%
-    map(~ mutate(.x, n_arms = length(unique(name)))) %>%
+  tbl_rewards <- pmap(
+    list(
+      tbl_conditions$rewards, 
+      as.list(tbl_conditions$var),
+      as.list(tbl_conditions$n_options)
+      ), 
+    ~ as.data.frame(..1) %>% mutate(var = ..2, n_arms = ..3)
+  ) %>%
+    map(~ pivot_longer(.x, -c(var, n_arms))) %>%
     reduce(rbind) %>% 
     mutate(name = substr(name, 2, 5))
   
   ggplot(tbl_rewards, aes(name, value, group = name)) +
     geom_violin(draw_quantiles = .5, aes(fill = name)) +
     scale_fill_brewer(name = "Option", palette = "Set1") +
-    facet_wrap(~ n_arms) + 
+    facet_grid(var ~ n_arms) + 
     theme_bw() +
     labs(x = "Option", y = "Reward")
 }
