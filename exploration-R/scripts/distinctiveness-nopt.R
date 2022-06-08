48987c760a38267afbb74bf53cf82a3a068be543
MirkoTh <mirkothalmann@hotmail.com>
2022-06-08 17:05:43 +0200

    add four response options

--- exploration-R/scripts/distinctiveness-nopt.R
+++ exploration-R/scripts/distinctiveness-nopt.R
@@ -1,56 +1,72 @@
 library(tidyverse)
 library(rutils)
 
+params_fixed <- list(
+  "n_trials" = 12,
+  "n_options_max" = 4,
+  "ri" = 2,
+  "v_means" = seq(1, n_options_max*2, by = 2),
+  "v_sd" = c(1),
+  "conditions" = fct_inorder(c("Massed", "Distinct I", "Distinct II"), ordered = TRUE)
+)
 
-n_trials <- 12
-n_options_max <- 4
-ri <- 2
-v_means <- seq(1, n_options_max, by = 2)
-v_sd <- c(1)
-conditions <- fct_inorder(c("Massed", "Distinct I", "Distinct II"), ordered = TRUE)
+
+
 
 # set up conditions table
-tbl_2 <- tibble(
-  trial_id = rep(
-    seq(1, n_trials, by = 1), length(conditions)),
-  condition = rep(conditions, each = n_trials),
-  option_selected = factor(c(
-    rep(c(1, 2), each = n_trials/2),
-    rep(c(1, 2), n_trials/2),
-    c(1, 1, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1)
-  )),
-  value_sampled = rnorm(v_means[option_selected], v_sd),
-  distance_t = abs(n_trials - trial_id) + ri,
-  distance_t_log = log(distance_t)
-)
-tbl_2 <- tbl_2 %>% group_by(condition, option_selected) %>%
-  arrange(trial_id) %>%
+
+make_condition_trials <- function(n_options, params_fixed, condition_distinct_ii) {
+  tibble(
+    trial_id = rep(
+      seq(1, params_fixed$n_trials, by = 1), length(params_fixed$conditions)),
+    condition = rep(params_fixed$conditions, each = params_fixed$n_trials),
+    setsize = n_options,
+    option_selected = factor(c(
+      rep(1:n_options, each = params_fixed$n_trials/n_options),
+      rep(1:n_options, params_fixed$n_trials/n_options),
+      condition_distinct_ii
+    )),
+    value_sampled = rnorm(params_fixed$v_means[option_selected], v_sd),
+    distance_t = abs(params_fixed$n_trials - trial_id) + ri,
+    distance_t_log = log(distance_t)
+  )
+}
+
+distinct_ii <- c(1, 1, 1, 2, 2, 1, 2, 2, 1, 2, 2, 1)
+tbl_2 <- make_condition_trials(2, params_fixed, distinct_ii)
+distinct_ii <- c(1, 2, 3, 4, 2, 3, 1, 4, 2, 3, 4, 1)
+tbl_4 <- make_condition_trials(4, params_fixed, distinct_ii)
+tbl_setsize <- rbind(tbl_2)
+
+
+tbl_setsize <- tbl_setsize %>% group_by(condition, setsize, option_selected) %>%
   mutate(trial_id_option_bw = row_number(desc(trial_id))) %>%
-  ungroup() %>% arrange(condition, option_selected)
-# 
-l_tbl_2 <- split(tbl_2, interaction(tbl_2$option_selected, tbl_2$condition))
-l_idx <- as.list(rep(c(1:nrow(l_tbl_2[[1]])), length(l_tbl_2)))
-l_map <- list(l_idx, rep(l_tbl_2, each = length(l_tbl_2)))
-calc_discriminability <- function(n, tb, c, alpha) {
+  ungroup() %>% arrange(setsize, condition, option_selected)
+l_tbl_setsize <- split(tbl_setsize, interaction(tbl_setsize$condition, tbl_setsize$setsize))
+# drop empty list elements
+l_tbl_setsize <- l_tbl_setsize[map_lgl(l_tbl_setsize, ~ nrow(.x) > 0)]
+calc_discriminability <- function(tb, c, alpha) {
   distances_all <- tb$distance_t_log
   # map over all distances besides n
   all_positions <- seq(1, length(distances_all), by = 1)
-  1/sum(map_dbl(distances_all, ~ exp(-c*abs(distances_all[n] - .x)^alpha)))
+  f_distinct <- function(n) 1/sum(map_dbl(distances_all, ~ exp(-c*abs(distances_all[n] - .x)^alpha)))
+  map_dbl(all_positions, f_distinct)
 }
 
-tbl_2$discriminability <- pmap_dbl(l_map, calc_discriminability, 1, 1)
+tbl_setsize$discriminability <- unlist(map(l_tbl_setsize, calc_discriminability, 1, 1))
 
-x11()
-ggplot(tbl_2, aes(trial_id, as.numeric(as.character(option_selected)), group = condition)) +
+ggplot(tbl_setsize, aes(trial_id, as.numeric(as.character(option_selected)), group = condition)) +
   geom_hline(yintercept = 1) +
   geom_hline(yintercept = 2) +
+  # geom_hline(yintercept = 3) +
+  # geom_hline(yintercept = 4) +
   geom_point(color = "white", size = 3) +
   geom_point(aes(color = condition)) +
-  facet_wrap(~ condition) +
+  facet_wrap(setsize ~ condition) +
   theme_bw() +
   scale_color_brewer(palette = "Set1", guide = "none") +
   scale_x_continuous(breaks = seq(1, n_trials, by = 2)) +
-  #scale_y_continuous(breaks = c(1, 2)) +
+  scale_y_continuous(breaks = c(1, 2)) + # , 3, 4
   coord_cartesian(ylim = c(0, 3)) +
   labs(
     x = "Trial ID",
@@ -58,21 +74,28 @@ ggplot(tbl_2, aes(trial_id, as.numeric(as.character(option_selected)), group = c
   )
 
 # plot discriminability of last three items, which we are going to ask people for
-tbl_last_3 <- tbl_2 %>% filter(trial_id_option_bw <= 3)
-ggplot(tbl_2, aes(trial_id, discriminability, group = condition)) +
+tbl_last_3 <- tbl_setsize %>% filter(trial_id_option_bw <= 3)
+ggplot(
+  tbl_setsize %>% filter(option_selected == 1) %>% mutate(
+    setsize = str_c("Set Size = ", setsize)
+  ) , aes(trial_id, discriminability, group = condition)) +
+  geom_line(aes(color = condition)) +
   geom_point(color = "white", size = 3) +
   geom_point(aes(color = condition)) +
-  facet_wrap(option_selected ~ condition) +
+  facet_wrap(setsize ~ condition) +
   theme_bw() +
   scale_color_brewer(palette = "Set1", guide = "none") +
   scale_x_continuous(breaks = seq(1, n_trials, by = 2)) +
   labs(
     x = "Trial ID",
-    y = "Discriminability in Memory"
+    y = "Discriminability in Memory",
+    title = "Response Option = 1"
   )
 
 pd <- position_dodge(width = .95)
-grouped_agg(tbl_last_3, c(condition, option_selected), discriminability) %>%
+grouped_agg(tbl_last_3  %>% mutate(
+  option_selected = str_c("Response Option = ", option_selected)
+  ), c(condition, option_selected), discriminability) %>%
   ggplot(aes(condition, mean_discriminability, group = option_selected)) +
   geom_col(aes(fill = condition), position = pd, alpha = .5) +
   geom_point(aes(color = condition), position = pd) +
