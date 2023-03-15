c197a5495b66a37051ab42b0840da7262065d822
Mirko Thalmann <mirkothalmann@hotmail.com>
2023-03-15 18:59:00 +0100

    plot random sample of participants

--- exploration-R/scripts/decision-strategies.R
+++ exploration-R/scripts/decision-strategies.R
@@ -62,10 +62,11 @@ tbl_cor %>%
 # rest is more or less ok
 
 
-strategies_one_subject <- function(subject_id) {
+
+strategies_one_subject <- function(subject_id, tbl_data) {
   m <- train(
     as.factor(repeat_choice) ~ nr_previous_switches + run_length + p_prev, #m_prev + v_prev,
-    data = tbl_e2_switches %>% filter(subject == subject_id),
+    data = tbl_data %>% filter(subject == subject_id),
     method = "rpart",
     trControl = trainControl(method = "cv", number = 10),
     tuneLength = 20
@@ -74,7 +75,7 @@ strategies_one_subject <- function(subject_id) {
 }
 
 subjs <- unique(tbl_e2_switches$subject)
-l_ms <- map(subjs, strategies_one_subject)
+l_ms <- map(subjs, strategies_one_subject, tbl_data = tbl_e2_switches)
 
 
 make_my_list <- function(x, y) {
@@ -89,11 +90,13 @@ tbl_results <- map2(l_ms, subjs, make_my_list) %>%
   reduce(rbind) %>% replace_na(list(value = 0)) %>%
   pivot_wider(id_cols = id, values_from = value, names_from = name)
 
+
 nrow <- 4
 ncol <- 3
+my_subjects_e2 <- sample(unique(tbl_e2_switches$subject), nrow * ncol, replace = FALSE)
 par(mfrow=c(nrow, ncol))
 for (i in 1:(nrow*ncol)){
-  rpart.plot(l_ms[[i]]$finalModel, cex = 1)
+  rpart.plot(l_ms[[my_subjects_e2[i]]]$finalModel, cex = 1)
 }
 
 
@@ -141,7 +144,8 @@ tbl_rb <- tbl_rb %>% select(-run_length) %>%
     nr_previous_switches = nr_previous_switches_lagged,
     repeat_choice = repeat_deck,
     m_prev = m_diff,
-    v_prev = v_diff
+    v_prev = v_diff,
+    subject = id2
     )
 
 tbl_cor <- cor(
@@ -178,46 +182,26 @@ tbl_cor %>%
 # var prev and pmu prev, but same as above
 # rest is more or less ok
 
-strategies_one_subject <- function(subject_id) {
-  m <- train(
-    as.factor(repeat_choice) ~ nr_previous_switches + run_length + p_prev, #m_prev + v_prev,
-    data = tbl_rb %>% filter(id2 == subject_id),
-    method = "rpart",
-    trControl = trainControl(method = "cv", number = 10),
-    tuneLength = 20
-  )
-  return(m)
-}
+
 
 # id2 == 1 selected the same deck throughout the experiment
-subjs <- unique(tbl_rb$id2)
+subjs <- unique(tbl_rb$subject)
 subjs <- subjs[subjs != 1]
-l_ms <- map(subjs, strategies_one_subject)
-
-
-make_my_list <- function(x, y) {
-  tbl_design <- tibble(name = c("p_prev", "run_length", "nr_previous_switches")) %>% mutate(id = y)
-  # tbl_design <- tibble(name = c("m_prev", "v_prev", "run_length", "nr_previous_switches")) %>% mutate(id = y)
-  tbl_results <- as_tibble(data.frame(id = y, t(x$finalModel$variable.importance))) %>% pivot_longer(cols = -id) %>% select(-id)
-  # tbl_results <- as_tibble(data.frame(id = y, t(x$finalModel$coefficients[2:4]))) %>% pivot_longer(cols = -id) %>% select(-id)
-  tbl_design %>% left_join(tbl_results, by = "name")
-}
-
-idx <- 10
-make_my_list(l_ms[[idx]], subjs[idx])
+l_ms_rb <- map(subjs, strategies_one_subject, tbl_data = tbl_rb)
 
 
 # for some participants the model does not converge, therefore use "safely"
-tbl_results <- map2(l_ms, subjs, safely(make_my_list)) %>%
+tbl_results <- map2(l_ms_rb, subjs, safely(make_my_list)) %>%
   map("result") %>%
   reduce(rbind) %>% replace_na(list(value = 0)) %>%
   pivot_wider(id_cols = id, values_from = value, names_from = name)
 
 nrow <- 4
 ncol <- 3
+my_subjects_rb <- sample(unique(tbl_results$id), nrow * ncol, replace = FALSE)
 par(mfrow=c(nrow, ncol))
 for (i in 1:(nrow*ncol)){
-  rpart.plot(l_ms[[i]]$finalModel, cex = 1)
+  rpart.plot(l_ms_rb[[my_subjects_rb[i]]]$finalModel, cex = 1)
 }
 
 
