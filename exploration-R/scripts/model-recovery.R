dd6bde70a39ece0d035c2f085d7f941f616d2d5f
Mirko Thalmann <mirkothalmann@hotmail.com>
2023-03-31 18:41:29 +0200

    initialize file for model recovery study

---
+++ exploration-R/scripts/model-recovery.R
@@ -0,0 +1,18 @@
+
+
+# simulate rewards of four armed restless bandit according to random walk
+# 1.
+# simulate choices according to soft max model with 0 variances
+# fit these choices with
+# soft max 0 variances
+# thompson 1 variance?
+# ucb 0 variances
+
+
+
+
+
+
+
+
+
