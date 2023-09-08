33325a46336ad4cac08cda15cabcd6dd28b60544
MirkoTh <mirkothalmann@hotmail.com>
2023-09-08 17:57:22 +0200

    generate fixed stimulus sets for the symmetry span task

--- experiments/2023-09-wm-batteries/wmc-battery/generate-stimuli.R
+++ experiments/2023-09-wm-batteries/wmc-battery/generate-stimuli.R
@@ -2,8 +2,12 @@ library(tidyverse)
 
 my_two_seeds <- c(39737632, 8567389)
 session_id <- 1
-
 set.seed(my_two_seeds[session_id])
+
+
+
+# Operation Span ----------------------------------------------------------
+
 
 maxSetSize <- 8
 minSetSize <- 4
@@ -43,8 +47,8 @@ df_lists <- map(l_letters_str, ~ str_c(str_replace_all(.x, "(,) $", replacement=
   reduce(rbind)
 rownames(df_lists) <- rep("", nrow(df_lists))
 l_lists <- str_c("[", reduce(str_c(reduce(df_lists, c), ", "), str_c), "]")
-str_c(str_replace(l_lists, "(, ')(\\])", replacement=, "\\2"), "]")
-
+tmp <- str_c(str_replace_all(l_lists, "(, ')(\\])", replacement=, "\\2"), "]")
+str_replace(tmp, "(, \\])(\\])$", replacement=, "\\2")
 
 eqsCorrect_str <- reduce(c("[", str_c(eqsCorrect, ", ")), str_c)
 str_c(str_replace_all(eqsCorrect_str, "(e)(,) $", replacement="\\1"), "]")
@@ -69,3 +73,94 @@ map(l_str, ~ str_c(str_replace(.x, ", $", replacement=""), "]"))
 
 
 
+# Symmetry Span -----------------------------------------------------------
+
+minSetSize <- 2 #// starting length of each trial (i.e., min number of letters in a trial)
+maxSetSize <- 5 #// ending length of each trial (i.e., max number of letters in a trial)
+repSet <- 3
+
+SetSizes <- rep(seq(minSetSize, maxSetSize, by = 1), repSet)
+demo_mem <- c(3, 4, 4)
+demo_comb <- c(3, 3, 4)
+SetSizes <- c(demo_mem, demo_comb, sample(SetSizes, length(SetSizes), replace = FALSE))
+
+matrix_pos <- crossing(row = seq(0, 3, by = 1), col = seq(0, 3, by = 1))
+locate <- function(x, mat) {
+  l_sample <- sample(1:nrow(mat), x, replace = FALSE)
+  d_prep <- mat[l_sample, ]
+  indices <- unlist(pmap(d_prep, ~ str_c("[", .x, ",", .y, "],")))
+  out <- str_c("[", str_replace(reduce(indices, str_c), ",$", ""), "]")
+  return(list(
+    d_prep, out
+  ))
+}
+l2_mat_full <- map(SetSizes, locate, mat = matrix_pos)
+l_matrix_pos <- map(l2_mat_full, 2)
+m_matrix_pos <- map(l2_mat_full, 1)
+out <- map(l_matrix_pos, ~ str_c("[", str_replace(reduce(.x, str_c), ",$", ""), "]"))
+l_matrix_pos <- str_c("[", reduce(str_c(reduce(l_matrix_pos, c), ", "), str_c), "]")
+
+
+half_matrix <- crossing(row = seq(0, 7, by = 1), col = seq(0, 3, by = 1))
+ns_dark_possible <- c(17, 18, 19)
+ns_dark <- sample(ns_dark_possible, sum(SetSizes), replace = TRUE)
+
+l2_half <- map(ns_dark, locate, mat = half_matrix)
+l_half_matrix_pos <- map(l2_half, 2)
+out <- map(l_half_matrix_pos, ~ str_c("[", str_replace(reduce(.x, str_c), ",$", ""), "]"))
+l_half_matrix_pos <- str_c("[", reduce(str_c(reduce(l_half_matrix_pos, c), ", "), str_c), "]")
+
+m_half_matrix_pos <- map(l2_half, 1)
+pSymmetric <- sample(c(0, 1), sum(SetSizes), replace = TRUE, prob = c(.5, .5))
+
+m_boxes_available <- map(
+  m_half_matrix_pos,
+  ~ left_join(half_matrix, .x %>% mutate(avl = 1), by = c("row", "col")) %>%
+    filter(is.na(avl)) %>% select(-avl)
+)
+
+numberBoxesDifferent <- sample(c(3, 4), sum(SetSizes), replace = TRUE)
+
+ns_remain <- ns_dark - numberBoxesDifferent
+
+l2_remain <- map2(ns_remain, m_half_matrix_pos, ~ locate(.x, .y))
+l_boxes_remain <- map(l2_remain, 2)
+m_boxes_remain <- map(l2_remain, 1)
+
+l2_change <- map2(numberBoxesDifferent, m_boxes_available, ~ locate(.x, .y))
+l_boxes_change <- map(l2_change, 2)
+m_boxes_change <- map(l2_change, 1)
+
+m_boxes_modified <- map2(m_boxes_remain, m_boxes_change, ~ rbind(.x, .y))
+indices <- map(m_boxes_modified, ~ unlist(pmap(.x, ~ str_c("[", .x, ",", .y, "],"))))
+out <- map(indices, ~ str_c("[", str_replace(reduce(.x, str_c), ",$", ""), "]"))
+l_boxes_modified <- str_c("[", reduce(str_c(reduce(out, c), ", "), str_c), "]")
+
+# setSizes_symm
+SetSizes_str <- reduce(c("[", str_c(SetSizes, ", ")), str_c)
+str_c(str_replace_all(SetSizes_str, "(,) $", replacement=""), "]")
+
+# blackBoxNumbers
+ns_dark_str <- reduce(c("[", str_c(ns_dark, ", ")), str_c)
+str_c(str_replace_all(ns_dark_str, "(,) $", replacement=""), "]")
+
+# blackBoxesAll
+str_replace(l_half_matrix_pos, "(, )(\\])$", replacement=, "\\2")
+# blackBoxesModified
+str_replace(l_boxes_modified, "(, )(\\])$", replacement=, "\\2")
+
+# pSymmetric
+pSymmetric_str <- reduce(c("[", str_c(pSymmetric, ", ")), str_c)
+str_c(str_replace_all(pSymmetric_str, "(,) $", replacement=""), "]")
+
+# selections_symm
+str_replace(l_matrix_pos, "(, )(\\])$", replacement=, "\\2")
+
+
+// prepare: 
+  // blackBoxNumbers samples from 17 - 19 (nr. trials * setSize + nr. practice trials)
+// blackBoxes: indices for matrix to select blackBoxes (nr. trials * setSize + nr. practice trials)
+// pSymmetric (nr. trials * setSize + nr. practice trials)
+// numberboxesDifferent samples from 3-4 (nr. trials * setSize + nr. practice trials)
+// boxes which 3-4 boxes to change subset from blackBoxes
+// boxesAdd which 3-4 boxes to change from white to black
